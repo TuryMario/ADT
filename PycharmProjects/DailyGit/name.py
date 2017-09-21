54da0fe89969b12f08ebf6fb75841a72bbb1fdc8
mariotury <mariotury@outlook.com>
2017-09-21 10:08:18 +0300

    first commit

---
+++ PycharmProjects/DailyGit/name.py
@@ -0,0 +1,9 @@
+def factors(n):  # traditional function that computes factors
+    results = []  # store factors in a new list
+    for k in range(1, n + 1):
+        if n % k == 0:  # divides evenly, thus k is a factor
+            results.append(k)  # add k to the list of factors
+    return results
+
+
+factors(60)
